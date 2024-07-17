ee61ee5a75e3ebbb67334f06096ff45a3371f4ad
Carlo Terzaghi Tuck Schneider <69093510+carloterzaghi@users.noreply.github.com>
2024-07-16 22:28:44 -0300

    Version 2.1
    -> Add fuction timer 
    -> Fixed F5 bug on the "machineOn" page
    -> Better fuction with SD Card Module on Web Page
    -> Better the CSS and fixed the CSS bug on phones

--- data/pages/sendWebSocketData.js
+++ data/pages/sendWebSocketData.js
@@ -2,6 +2,7 @@ var Socket;
 document.getElementById('BTN_SEND_BACK').addEventListener('click', button_send_back);
 const gravityInput = document.getElementById('gravity');
 var fileNameToSend = "";
+var startTimer = "";
 
 function connectWebSocket() {
   try {
@@ -34,6 +35,7 @@ function button_send_back() {
   console.log(gravityInput.value);
   Socket.send(gravityInput.value);
 }
+
 function processCommand(event) {
 
   const dadosSeparados = event.data.split(',');
@@ -43,13 +45,15 @@ function processCommand(event) {
   const buttonState = dadosSeparados[1];
   const randomValue = dadosSeparados[2];
   const gravity = dadosSeparados[3];
-
+  const timerValue = dadosSeparados[4];
 
   // Edita o Start se não tiver conectado o cartão SD no ESP32
   if (document.body.id === 'PageOff') {
+    startTimer = "";
     const startButton = document.getElementById('BTN_SEND_BACK');
     const sdButton = document.getElementById('sdButtonID');
     const filenameInput = document.getElementById('filename');
+    const timeInput = document.getElementById('time');
 
     console.log(sdExist);
     if (String(sdExist) === '1') {
@@ -59,13 +63,21 @@ function processCommand(event) {
       sdButton.onclick= function(){
         location.href='/sd/data'
       }
-      console.log(gravityInput.value);
-      console.log(filenameInput.value);
       startButton.style.backgroundColor = "#50C878";
       if (gravityInput.value != "" && filenameInput.value != ""){
+        // Se o timeInput não for mexido o dado não será relevado
+        if (String(timeInput.value) == "00:00:00") TimeSet = "";
+        else TimeSet = String(timeInput.value);
+
+        // Verifica se o dado está em hh:mm, se sim adiciona no final um :00 para ficar hh:mm:ss
+        if (/^([01]\d|2[0-3]):([0-5]\d)$/.test(TimeSet)) {
+          TimeSet += ":00"; // Adiciona :00 para os segundos
+        }
+
+        // Pega o dado do nome do arquivo para ser enviado por via URI
         fileNameToSend = String(filenameInput.value);
         startButton.onclick= function(){
-          location.href=`/machine/on?filename=${encodeURIComponent(fileNameToSend.replace(/ /g, "_"))}`
+          location.href=`/machine/on?filename=${encodeURIComponent(fileNameToSend.replace(/ /g, "_"))}&timer=${encodeURIComponent(TimeSet)}`
         }
       }
       else{
@@ -85,6 +97,15 @@ function processCommand(event) {
     }
   }
   else{
+    // Verifica se existe o Timer setado, se existir um valor para timerValue da um valor ao startTimer e exibe o timer 
+    if (timerValue != ""){
+      if (startTimer == ""){
+        document.getElementById('counterTitle').textContent = "Countdown Timer";
+        startTimer = timerValue; // timerValue está em hh:mm:ss // 
+        document.getElementById('countdown').innerHTML = startTimer;
+      }
+    }
+
     // Atualizar os elementos da página
     if (gravity != ""){
       document.getElementById('buttonState').innerHTML = buttonState;
